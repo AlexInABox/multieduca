92fc65b8c56aad7a89fabc181190975c89e52116
AlexInABox <36812824+AlexInABox@users.noreply.github.com>
2023-02-06 16:10:20 +0000

    fix client logic

--- test/client.java
+++ test/client.java
@@ -5,27 +5,36 @@ public class client {
 
     public static void main(String[] args) throws IOException {
 
-        Socket s = new Socket("https://alexinabox-solid-carnival-pg9jq5qpgrwh7jpq.github.dev", 4000);
+        Socket s = new Socket("cloud.alexinabox.de", 4000);
 
         System.out.println("Connection established");
 
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
 
-        System.out.println("Waiting for host to send message");
+
         PrintWriter pr = new PrintWriter(s.getOutputStream());
+        System.out.println("Sending message to host");
         pr.println("Hello Host, I am client!");
         pr.flush();
+        System.out.println("Sent message to host");
+        
         while (true) {
 
             String str = bf.readLine();
             handleMessage(str);
+            //wait 5 seconds
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             respondToHost(pr);
         }
     }
 
     public static void handleMessage(String msg) {
-        System.out.println("host: " + msg.toString());
+        System.out.println("host: " + msg);
     }
 
     public static void respondToHost(PrintWriter pr) {
