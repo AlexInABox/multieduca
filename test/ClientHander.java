72a23934a82ec823ecf50aabf187f7018f2a21b4
MaximalEinBier <125266657+MaximalEinBier@users.noreply.github.com>
2023-02-13 12:15:55 +0100

    Add files via upload

---
+++ test/ClientHander.java
@@ -0,0 +1,12 @@
+import java.net.*;
+import java.io.*;
+
+class ClientHander extends host{
+    private Socket clientSocket;
+
+    public ClientHandeler(Socket socket){
+    
+    this.clientSocket = socket;
+    
+    }
+}
