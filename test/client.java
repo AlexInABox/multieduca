e9faad43b9c26c1503887c5b4e2d93118a825f3a
AlexInABox <main@alexinabox.de>
2023-02-18 01:17:36 +0100

    merge newlcient into client

--- test/client.java
+++ test/client.java
@@ -2,45 +2,83 @@ import java.net.*;
 import java.io.*;
 
 public class client {
+    // dev variables
+    private static final String IP = "localhost";
+    // end of dev variables
+    // pre defined variables
+    private static final int PORT = 2594;
+    // end of pre defined variables
+
+    // networking related variables
+    private static Socket s;
     private static PrintWriter pr;
+    private static InputStreamReader in;
+    private static BufferedReader bf;
+    // end of networking related variables
 
-    public static void main(String[] args) throws IOException {
+    // game related variables
+    private static String nick = "Lorem Ipsum";
+    private static int punktezahl = 0;
+    private static String question; // the question and the answer are both provided in the JSON format
+    private static int answer = 0;
+    private static boolean result;
+    // end of game related variables
 
-        Socket s = new Socket("10.16.111.109", 2594);
-
-        System.out.println("Connection established with: " + s.getInetAddress().getHostAddress() + ":" + s.getPort());
-
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
-        BufferedReader bf = new BufferedReader(in);
-
-        pr = new PrintWriter(s.getOutputStream());
-        System.out.println("Sending name to host");
-        pr.println("Alex");
-        pr.flush();
-        System.out.println("Sent message to host");
-
-        while (true) {
-
-            String str = bf.readLine();
-            if (str == null)
-                break;
-            handleMessage(str);
-        }
+    public static void main(String[] args) {
+        connectToHost(IP);
     }
 
-    public static void handleMessage(String msg) {
-        System.out.println("host: " + msg);
-
+    public static void connectToHost(String ip) {
         try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            establishConnection(ip);
+            registerClient();
+
+            while (true) {
+                // wait for the round to start
+                // if the host send the start signal, the client will prepare to receive the
+                // question
+                // if the host send the end signal, the client will break the loop and end the
+
+                if (bf.readLine().equals("START ROUND")) {
+                    // receive the question
+                    question = bf.readLine();
+                    // give the question to the function that will **eventually return the answer**
+                    answer = dummy.roundStarted(question);
+                    // send the answer
+                    pr.println(answer);
+                    pr.flush();
+                } else if (bf.readLine().equals("RESULT")) {
+                    // receive the result
+                    result = Boolean.parseBoolean(bf.readLine()); // Example: Boolean.parseBoolean("True") returns true.
+                                                                  // Example: Boolean.parseBoolean("yes") returns false.
+                    // receive the points
+                    punktezahl = Integer.parseInt(bf.readLine()); // Example: Integer.parseInt("123") returns 123.
+
+                    // give the result and the points to the function that will handle those
+                    dummy.roundEnded(result, punktezahl);
+
+                } else if (bf.readLine().equals("END GAME")) {
+                    break;
+                }
+            }
+            // once the loop is broken, the client will close the connection
+            s.close();
+            System.out.println("Connection closed");
+        } catch (Exception e) {
+            System.out.println("OhNo, an error occured: " + e);
         }
-        sendMessage("My Answer is: fancy!");
     }
 
-    public static void sendMessage(String msg) {
-        pr.println(msg);
+    private static void establishConnection(String ip) throws Exception {
+        s = new Socket(ip, PORT);
+        pr = new PrintWriter(s.getOutputStream());
+        in = new InputStreamReader(s.getInputStream());
+        bf = new BufferedReader(in);
+    }
+
+    private static void registerClient() throws Exception {
+        // send the nickname
+        pr.println(nick);
         pr.flush();
     }
 }
