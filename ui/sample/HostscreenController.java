1613c1369bd702ebff79743d5b47e51545f1e582
AlexInABox <main@alexinabox.de>
2023-02-27 17:03:59 +0100

    add javafx to lib

--- ui/sample/HostscreenController.java
+++ ui/sample/HostscreenController.java
@@ -20,29 +20,29 @@ public class HostscreenController {
 
     }
 
-    public void switchToStartpage(ActionEvent event) throws IOException{
+    public void switchToStartpage(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../rsc/Startscreen.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
 
     }
-    public void switchToSpielErstellen(ActionEvent event) throws IOException{
+
+    public void switchToSpielErstellen(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../rsc/SpielErstellen.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
 
     }
 
-    public void openFile(ActionEvent event) throws IOException{
+    public void openFile(ActionEvent event) throws IOException {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Quiz Öffnen");
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Quizdatei", "*.json")
-        );
+                new FileChooser.ExtensionFilter("Quizdatei", "*.json"));
         fileChooser.showOpenDialog(stage);
     }
 
