425fb36ec4362a82f9312dbeae4ab22ffc0cd8bc
Basim <basimbennaji@gmail.com>
2023-03-03 15:04:21 +0100

    new WinSize, other improvments

--- ui/sample/Main.java
+++ ui/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../rsc/Startscreen.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 640, 400));
+        primaryStage.setScene(new Scene(root, 800, 550));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
