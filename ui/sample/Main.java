921dbcc61697284073deda75a265ff46ccc7ec62
moritzoehme <83780445+moritzoehme@users.noreply.github.com>
2023-03-09 10:37:44 +0100

    Pop Up Fenster bei Beenden

--- ui/sample/Main.java
+++ ui/sample/Main.java
@@ -15,8 +15,19 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 640, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
+
+        stage.setOnCloseRequest(event -> beenden(stage));
     }
 
+    public void beenden(Stage stage){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Beenden");
+        alert.setHeaderText("Du bist dabei, das Spiel zu beenden.");
+        alert.setContentText("Willst du fortfahren?");
+        if(alert.showAndWait().get() == ButtonType.OK){
+            stage.close();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
