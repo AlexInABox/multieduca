567c2164362a4795e486521b72b47a00c95231ca
niklas56 <122108332+niklas56@users.noreply.github.com>
2023-03-12 13:44:41 +0100

    fix

---
+++ ui/sample/MidRanking.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.SVGPath;
+
+public class MidRanking {
+
+    @FXML
+    private Pane ergebnisPane;
+
+    @FXML
+    private Label ergebnisText, position, punkte;
+
+    @FXML
+    void initialize() {
+
+    }
+}
