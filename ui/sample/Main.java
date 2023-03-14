e6f298c41cddf30c50a028ac65f994481c0026a0
AlexInABox <main@alexinabox.de>
2023-03-14 21:48:46 +0100

    fix

--- ui/sample/Main.java
+++ ui/sample/Main.java
@@ -31,7 +31,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../rsc/Startscreen.fxml"));
-        primaryStage.setTitle("Quiz Game");
+        primaryStage.setTitle("Multieduca");
         primaryStage.setScene(new Scene(root, 800, 550));
         primaryStage.setResizable(false);
         primaryStage.show();
