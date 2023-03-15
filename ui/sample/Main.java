f7a793263dd7d1a7ca5fbcd35116bf5624a747b0
AlexInABox <main@alexinabox.de>
2023-03-15 23:32:48 +0100

    comments main

--- ui/sample/Main.java
+++ ui/sample/Main.java
@@ -84,12 +84,8 @@ public class Main extends Application {
             String message, String optOutMessage, Consumer<Boolean> optOutAction,
             ButtonType... buttonTypes) {
         Alert alert = new Alert(type);
-        // Need to force the alert to layout in order to grab the graphic,
-        // as we are replacing the dialog pane with a custom pane
         alert.getDialogPane().applyCss();
         Node graphic = alert.getDialogPane().getGraphic();
-        // Create a new dialog pane that has a checkbox instead of the hide/show details button
-        // Use the supplied callback for the action of the checkbox
         alert.setDialogPane(new DialogPane() {
             @Override
             protected Node createDetailsButton() {
@@ -101,11 +97,8 @@ public class Main extends Application {
         });
         alert.getDialogPane().getButtonTypes().addAll(buttonTypes);
         alert.getDialogPane().setContentText(message);
-        // Fool the dialog into thinking there is some expandable content
-        // a group won't take up any space if it has no children
         alert.getDialogPane().setExpandableContent(new Group());
         alert.getDialogPane().setExpanded(true);
-        // Reset the dialog graphic using the default style
         alert.getDialogPane().setGraphic(graphic);
         alert.setTitle(title);
         alert.setHeaderText(headerText);
