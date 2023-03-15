28ed18de4e14ec75a8e2e5f2603325dc52c1996b
AlexInABox <main@alexinabox.de>
2023-03-15 21:44:02 +0100

    fix bat

--- multieduca.bat
+++ multieduca.bat
@@ -1,1 +1,1 @@
-java --module-path "C:\Users\beann\VSCODE\TEST\multieduca\lib\javafx-sdk-19.0.2.1\lib" --add-modules javafx.controls,javafx.fxml -jar .\multieduca.jar
+java --module-path lib/javafx-sdk-19.0.2.1/lib --add-modules javafx.controls,javafx.fxml --enable-preview -jar multieduca.jar
